eb4930699bfe92c9f007c41e2ad0805747ee8e40
JimmyPV <jaime.pvillena@gmail.com>
2023-04-27 16:56:04 +0200

    Threads (hilos por donde bajan los enemigos
    2 pngs
    gameScreen modificado

--- js/gameScreen.js
+++ js/gameScreen.js
@@ -7,20 +7,61 @@ let gameState = {
     update: gameUpdate
 };
 
+let platform;
+let threads;
+let n_webs = 10;
+let x_thread = 800/n_webs;
+
 game.state.add('menu', startState);
 game.state.add('game', gameState);
 //game.state.add('final', finalState);
 //game.state.add('options', optionState);
 
-game.state.start('menu');
+game.state.start('game');
 
 function loadAssets() {
-
+    console.log('arrancando');
+    game.load.image('sky', 'assets/sky.png');
+    game.load.image('ground', 'assets/ground.png');
+    game.load.image('thread', 'assets/string.png');
+    //test
+    game.load.image('character', 'assets/playButton.png');
 }
 function initialiseGame(){
+    game.physics.startSystem(Phaser.Physics.ARCADE);
 
+    game.add.sprite(0, 0, 'sky');
+
+    platform = game.add.group();
+    platform.enableBody = true;
+    
+    let ground = platform.create(0, game.world.height - 32, 'ground');
+    ground.body.immovable = true;
+
+    threads = game.add.group();
+    threads.enableBody = true;
+
+    thread_creator(n_webs);
+
+    let character = create(thread_pos_array[0], 300, 'character');
 }
 
 function gameUpdate(){
     
+}
+
+function thread_creator(n_webs){
+    let thread_pos = game.world.width/n_webs;
+    let thread_pos_array = [thread_pos];
+
+    for (let i = 1; i < n_webs; i++) {
+        const actual_thread = i;
+        
+        let web_thread = threads.create(thread_pos, 0, 'thread');
+        web_thread.body.immovable = true;
+
+        thread_pos = thread_pos + (game.world.width/n_webs);
+        thread_pos_array.push(thread_pos);
+    }
+    console.log(thread_pos_array);
 }
