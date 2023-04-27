de02f78ccca63cc7abd6bafda80d531ef7e0acb5
unknown <al397403@uji.es>
2023-04-27 13:05:04 +0200

    assets prueba

--- js/Menu.js
+++ js/Menu.js
@@ -1,4 +1,4 @@
-const { Button } = require("phaser-ce");
+
 
 //let GAME_AREA_WIDTH = window.innerWidth * window.devicePixelRatio;
 //let GAME_AREA_HEIGHT = window.innerHeight * window.devicePixelRatio;
@@ -20,18 +20,21 @@ game.state.add('options', optionState);
 
 game.state.start('menu');
 
-///PRELOAD///
+
 
 function preloadAssets(){
-    game.load.spritesheet('playButton', 'assets/playButton.png', 336, 158, 2);
+    juego.load.image('sky', 'assets/sky.png');
+    //game.load.spritesheet(juego.world.centerX,juego.world.centerY ,'playButton', 'assets/playButton.png',336, 158, 2);
 }
 
 function initializeGame(){
-    buttonPlay = new Button(0.5,0.45,'playButton', changePlay);
+    juego.add.sprite(0, 0, 'sky');
+    //buttonPlay = new Button(0.5,0.45,'playButton', changePlay);
+    //playButton = game.add.button(game.world.centerX, game.world.centerY, 'playButton', this.changePlay, this);
 }
 
 function changePlay(){
-game.state.start('main');
+game.state.start('game');
 }
 
 function updateGame(){
