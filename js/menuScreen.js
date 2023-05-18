3b48993bdf5024d4cacf0347a7ba42a72757780e
alberto2114 <90263353+alberto2114@users.noreply.github.com>
2023-05-18 17:33:46 +0200

    soy un manegueta

--- js/menuScreen.js
+++ js/menuScreen.js
@@ -64,7 +64,7 @@ function initializeGame() {
     //gear button
     options = game.add.sprite(game.world.width -42, 8, 'options'); //options icon
     options.scale.setTo(0.5);
-    options.inputEnabled = false;
+    options.inputEnabled = true;
     //options.events.onInputOver.loadTexture('menuBox'); trying some thing XD
     options.events.onInputDown.add(showMenu, this);
     //options panel
