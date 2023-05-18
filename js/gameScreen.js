7506aeed9a7e12e548975128815335a098c042df
Paumarpa <al397403@uji.es>
2023-05-18 18:56:01 +0200

    detecta hilo

--- js/gameScreen.js
+++ js/gameScreen.js
@@ -11,7 +11,7 @@ let platform;
 let threads;
 let threads_inclined;
 let thread_changer;
-let x_thread = 800/n_webs;
+let x_thread = 800 / n_webs;
 
 let thread_pos_array;
 let thread_inclined_array_ini;
@@ -29,14 +29,14 @@ const VELOCIDAD_DISPARO = 200;
 let fireButton;
 
 let enemies;
+let EThread;
 
 
 const NUM_LEVELS = 3;
-const LEVEL_ENEMY_SPAWN_PROB =[0.5, 0.75, 1];
-const LEVEL_ENEMY_VELOCITY =[200, 300, 350];
+const LEVEL_ENEMY_SPAWN_PROB = [0.5, 0.75, 1];
+const LEVEL_ENEMY_VELOCITY = [200, 300, 350];
 const SCORE_TO_NEXT_LEVEL = 50;
 let heartLives;
-const ASTEROID_VEL = 200;
 
 var tiempoTexto;
 var tiempoTranscurrido = 0;
@@ -64,7 +64,7 @@ function loadAssets() {
     game.load.image('thread', 'assets/string.png');
     game.load.image('purple', 'assets/trajectory_changer.png');
     //game.load.image('character', 'assets/spriteCharacter.png');
-    game.load.spritesheet('character', 'assets/spriteSheet.png', 198.5, 211 );
+    game.load.spritesheet('character', 'assets/spriteSheet.png', 198.5, 211);
 
 
     game.load.image('disparo', 'assets/ammo.png');
@@ -74,21 +74,21 @@ function loadAssets() {
 
 
 
-function initialiseGame(){
+function initialiseGame() {
     game.physics.startSystem(Phaser.Physics.ARCADE);
 
     game.add.sprite(0, 0, 'sky');
-    
+
     //esconder raton
     //this.input.mouse.disableContextMenu();
-   // this.input.mouse = this.input.mousePointer = this.input.addPointer(1);
+    // this.input.mouse = this.input.mousePointer = this.input.addPointer(1);
 
     platform = game.add.group();
     platform.enableBody = true;
-    
+
     cursors = game.input.keyboard.createCursorKeys();
     fireButton = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
-    
+
     let ground = platform.create(0, game.world.height - 32, 'ground');
     ground.body.immovable = true;
 
@@ -96,28 +96,30 @@ function initialiseGame(){
     threads.enableBody = true;
     thread_creator(n_webs);
 
-    threads_inclined = game.add.group();
+    //threads_inclined = game.add.group();
     //threads_inclined.enableBody = true;
 
-    thread_changer = game.add.group();
-    thread_changer.enableBody = true;
+    thread_changer_init = game.add.group();
+    thread_changer_init.enableBody = true;
 
+    thread_changer_end = game.add.group();
+    thread_changer_end.enableBody = true;
     thread_creator_V2();
 
     characterIndex = 0;
 
-    
-    
-    character = game.add.sprite(thread_pos_array[characterIndex]-37,game.world.height - 101,'character');
-    character.scale.setTo(0.5,0.5);
+
+
+    character = game.add.sprite(thread_pos_array[characterIndex] - 37, game.world.height - 101, 'character');
+    character.scale.setTo(0.5, 0.5);
     game.physics.arcade.enable(character);
-    
+
     character.animations.add('idle', [0, 1], 2.5, true);
 
-    tiempoTexto = this.add.text(3,10, "00:00:00", {font: "20px Arial", fill: "white", stroke: "black", strokeThickness:4});
-    textoPuntuaje = this.add.text(3,40, "Points: 0", {font: "20px Arial", fill: "white", stroke: "black", strokeThickness:4});
-    textoParte = this.add.text(739,10, "Part A", {font: "20px Arial", fill: "white", stroke: "black", strokeThickness:4});
-    textoLevel = this.add.text(746,40, "Lvl " + level, {font: "20px Arial", fill: "white", stroke: "black", strokeThickness:4});
+    tiempoTexto = this.add.text(3, 10, "00:00:00", { font: "20px Arial", fill: "white", stroke: "black", strokeThickness: 4 });
+    textoPuntuaje = this.add.text(3, 40, "Points: 0", { font: "20px Arial", fill: "white", stroke: "black", strokeThickness: 4 });
+    textoParte = this.add.text(739, 10, "Part A", { font: "20px Arial", fill: "white", stroke: "black", strokeThickness: 4 });
+    textoLevel = this.add.text(746, 40, "Lvl " + level, { font: "20px Arial", fill: "white", stroke: "black", strokeThickness: 4 });
 
     crearDisparos(DISPAROS_GROUP_SIZE);
 
@@ -137,61 +139,61 @@ function initialiseGame(){
     crono = setInterval(actualizarCronometro, 1000);
 }
 function spawnEnemies() {
-    if(Math.random() < LEVEL_ENEMY_SPAWN_PROB[level-1]){
-        let randomIndex = Math.floor(Math.random() * (n_webs-1));
-    let randomThread = thread_pos_array[randomIndex];
+    if (Math.random() < LEVEL_ENEMY_SPAWN_PROB[level - 1]) {
+        let randomIndex = Math.floor(Math.random() * (n_webs - 1));
+        let EThread = thread_pos_array[randomIndex];
 
-    let enemy = enemies.create(randomThread, 0, 'asteroid');
-    enemy.scale.setTo(1, 1);
-    enemy.anchor.setTo(0.5, 0.5);
-    
-    enemy.body.velocity.y = LEVEL_ENEMY_VELOCITY[level-1];
+        let enemy = enemies.create(EThread, 0, 'asteroid');
+        enemy.scale.setTo(1, 1);
+        enemy.anchor.setTo(0.5, 0.5);
+
+        enemy.body.velocity.y = LEVEL_ENEMY_VELOCITY[level - 1];
     }
-    
+
     //enemy.body.angularVelocity = 150;
 }
 
 function spawnLives() {
-    let randomIndex = Math.floor(Math.random() * (n_webs-1));
+    let randomIndex = Math.floor(Math.random() * (n_webs - 1));
     let randomThread = thread_pos_array[randomIndex];
 
     let lives = heartLives.create(randomThread, 0, 'heart');
     lives.scale.setTo(0.02, 0.02);
     lives.anchor.setTo(0.10, 0.5);
-    
-    lives.body.velocity.y = ASTEROID_VEL;
+
+    lives.body.velocity.y = LEVEL_ENEMY_VELOCITY[level - 1];
 }
 
-function crearDisparos(num){
+function crearDisparos(num) {
     disparos = game.add.group();
     disparos.enableBody = true;
-    disparos.createMultiple(num,'disparo');
+    disparos.createMultiple(num, 'disparo');
     //disparos.callAll('events.onOutofBounds.add','events.onOutOfBounds',resetMember);
-    disparos.forEach(function(disparo){
-        disparo.scale.setTo(0.25,0.25);
+    disparos.forEach(function (disparo) {
+        disparo.scale.setTo(0.25, 0.25);
     });
-    
-    
-    disparos.setAll('outOfBoundsKill',true);
-    disparos.setAll('checkWorldBounds',true);
+
+
+    disparos.setAll('outOfBoundsKill', true);
+    disparos.setAll('checkWorldBounds', true);
 }
 
 function resetMember(item) {
     item.kill();
-    }
+}
 
-function actualizarCronometro(){
+function actualizarCronometro() {
     tiempoTranscurrido++;
-    var horas = Math.floor(tiempoTranscurrido/3600);
-    var minutos = Math.floor((tiempoTranscurrido-(horas * 3600))/60);
-    var segundos = Math.floor(tiempoTranscurrido - (horas*3600)-(minutos * 60));
-    var tiempoTextoFormateado = horas.toString().padStart(2, "0")+":"+minutos.toString().padStart(2, "0")+":"+segundos.toString().padStart(2, "0");
+    var horas = Math.floor(tiempoTranscurrido / 3600);
+    var minutos = Math.floor((tiempoTranscurrido - (horas * 3600)) / 60);
+    var segundos = Math.floor(tiempoTranscurrido - (horas * 3600) - (minutos * 60));
+    var tiempoTextoFormateado = horas.toString().padStart(2, "0") + ":" + minutos.toString().padStart(2, "0") + ":" + segundos.toString().padStart(2, "0");
     tiempoTexto.setText(tiempoTextoFormateado);
 }
 
 
 
-    
+
 
 function updateHealthBar() {
     healthBar.querySelector('.bar').style.width = health + '%';
@@ -199,14 +201,14 @@ function updateHealthBar() {
 }
 function decreaseHealthBar(enemy) {
     enemy.kill();
-    health-=20;
+    health -= 20;
     var damageAudio = new Audio("assets/songs/damage.mp3");
     var failAudio = new Audio("assets/songs/Fail.mp3");
-    if (health<=0){
+    if (health <= 0) {
         damageAudio.play();
         gameEnd = true;
     }
-    else{
+    else {
         damageAudio.play();
         updateHealthBar();
         console.log("la barra de vida tiene " + health);
@@ -215,62 +217,63 @@ function decreaseHealthBar(enemy) {
 }
 
 
-function gameUpdate(){
+function gameUpdate() {
     //collisions
-    game.physics.arcade.overlap(enemies,disparos,enemyHit,null,this);
-    game.physics.arcade.overlap(enemies,platform,decreaseHealthBar,null,this);
-    game.physics.arcade.overlap(heartLives,character,liveHit,null,this);
-    
+    game.physics.arcade.overlap(enemies, disparos, enemyHit, null, this);
+    game.physics.arcade.overlap(enemies, platform, decreaseHealthBar, null, this);
+    game.physics.arcade.overlap(heartLives, character, liveHit, null, this);
+    game.physics.arcade.overlap(enemies, thread_changer_init, changeThread, null, this);
+
     character.animations.play('idle');
 
-    if(boolmouse){
+    if (boolmouse) {
         //movimiento con raton
-            mouseX = game.input.mousePointer.x;
-            let relativePos = mouseX - thread_pos_array[characterIndex]+(x_thread/2);
-            if (relativePos < 0){
-                if(characterIndex > 0 ){      
-                    characterIndex--;
-                    character.body.position.setTo(thread_pos_array[characterIndex]-37,game.world.height - 101 );
+        mouseX = game.input.mousePointer.x;
+        let relativePos = mouseX - thread_pos_array[characterIndex] + (x_thread / 2);
+        if (relativePos < 0) {
+            if (characterIndex > 0) {
+                characterIndex--;
+                character.body.position.setTo(thread_pos_array[characterIndex] - 37, game.world.height - 101);
 
 
-                }
             }
-            else if( relativePos > (thread_pos_array[characterIndex + 1] - thread_pos_array[characterIndex])){
-                if(characterIndex < n_webs -2) {
+        }
+        else if (relativePos > (thread_pos_array[characterIndex + 1] - thread_pos_array[characterIndex])) {
+            if (characterIndex < n_webs - 2) {
                 characterIndex++;
-                character.body.position.setTo(thread_pos_array[characterIndex]-37,game.world.height - 101 );
+                character.body.position.setTo(thread_pos_array[characterIndex] - 37, game.world.height - 101);
 
-                }
             }
+        }
     }
     //movimiento con flchas
-    else{
-        if(cursors.left.isDown && characterIndex > 0 && freeInput ==true){
+    else {
+        if (cursors.left.isDown && characterIndex > 0 && freeInput == true) {
             //left movement
             console.log('left');
             characterIndex--;
-            character.body.position.setTo(thread_pos_array[characterIndex]-37,game.world.height - 101 );
+            character.body.position.setTo(thread_pos_array[characterIndex] - 37, game.world.height - 101);
 
-            freeInput=false;
-            game.time.events.add(200, inputChorno,this);
-        } else if(cursors.right.isDown && characterIndex < n_webs-2 && freeInput ==true){
+            freeInput = false;
+            game.time.events.add(500/n_webs, inputChorno, this);
+        } else if (cursors.right.isDown && characterIndex < n_webs - 2 && freeInput == true) {
             //right movement
             console.log('right');
             characterIndex++;
-            character.body.position.setTo(thread_pos_array[characterIndex]-37,game.world.height - 101 );
+            character.body.position.setTo(thread_pos_array[characterIndex] - 37, game.world.height - 101);
             character.animations.play('character2');
-            freeInput=false;
-            game.time.events.add(200, inputChorno,this);
-    
+            freeInput = false;
+            game.time.events.add(500/n_webs, inputChorno, this);
+
         }
-    } 
+    }
     manageShots();
-    if(gameEnd){
+    if (gameEnd) {
         gameOver();
     }
 }
 
-function gameOver(){
+function gameOver() {
 
     health = 0;
     clearInterval(crono);
@@ -284,20 +287,24 @@ function gameOver(){
     game.state.start('menu');
 }
 
-function enemyHit(enemy,disparo){
+function changeThread(enemy, thread) {
+    console.log("changeThread",thread.myValue);
+}
+
+function enemyHit(enemy, disparo) {
     disparo.kill();
     enemy.kill();
     sumarPuntos();
     var boomAudio = new Audio("assets/songs/boom.mp3");
     boomAudio.play();
 
-    if(level < NUM_LEVELS && puntuaje== level*SCORE_TO_NEXT_LEVEL){
+    if (level < NUM_LEVELS && puntuaje == level * SCORE_TO_NEXT_LEVEL) {
         subirLevel();
     }
 
 }
 
-function liveHit(lives,character){
+function liveHit(lives, character) {
     character.kill();
     health = 100;
     updateHealthBar();
@@ -306,54 +313,54 @@ function liveHit(lives,character){
 
 }
 
-function manageShots(){
-    if(fireButton.justDown && !boolmouse){
+function manageShots() {
+    if (fireButton.justDown && !boolmouse) {
         fireShot();
     }
 
-    else if(game.input.mousePointer.leftButton.justPressed(30) && boolmouse){
+    else if (game.input.mousePointer.leftButton.justPressed(30) && boolmouse) {
         fireShot();
     }
 }
 
-function fireShot(){
-    let shotX = character.x+9 + character.width/4;
-    let shotY = character.y+-32;
+function fireShot() {
+    let shotX = character.x + 9 + character.width / 4;
+    let shotY = character.y + -32;
     let shotVel = -VELOCIDAD_DISPARO;
-    let shot = kaboom(shotX, shotY,shotVel);
+    let shot = kaboom(shotX, shotY, shotVel);
     var shootAudio = new Audio("assets/songs/Shoot.mp3");
     shootAudio.play();
 }
 
-function kaboom(x,y,vel){
+function kaboom(x, y, vel) {
     let shot = disparos.getFirstExists(false);
-    if(shot){
-        shot.reset(x,y);
+    if (shot) {
+        shot.reset(x, y);
         shot.body.velocity.y = vel;
     }
     return shot;
 }
 
-function inputChorno(){
-freeInput = true;
+function inputChorno() {
+    freeInput = true;
 }
 
 //Alberto
-function sumarPuntos(){
-    puntuaje +=10;
-    textoPuntuaje.setText("Points: "+puntuaje);
-    
+function sumarPuntos() {
+    puntuaje += 10;
+    textoPuntuaje.setText("Points: " + puntuaje);
+
 }
 
-function subirLevel(){
-    level+=1
-    textoLevel.setText('Lvl '+ level);
+function subirLevel() {
+    level += 1
+    textoLevel.setText('Lvl ' + level);
     var levelAudio = new Audio("assets/songs/levelUp.mp3");
     levelAudio.play();
 }
 
-function thread_creator(n_webs){
-    let thread_pos = game.world.width/n_webs;
+function thread_creator(n_webs) {
+    let thread_pos = game.world.width / n_webs;
     thread_pos_array = [thread_pos];
 
     for (let i = 1; i < n_webs; i++) {
@@ -361,36 +368,40 @@ function thread_creator(n_webs){
         web_thread.anchor.setTo(0.5, 0);
         web_thread.body.immovable = true;
 
-        thread_pos = thread_pos + (game.world.width/n_webs);
+        thread_pos = thread_pos + (game.world.width / n_webs);
         thread_pos_array.push(thread_pos);
     }
     console.log(n_webs);
 }
 
-function thread_creator_V2(){
-    let catetoX = 800/n_webs;
+function thread_creator_V2() {
+    let catetoX = 800 / n_webs;
     let catetoY;
     thread_inclined_array_ini = [];
     thread_inclined_array_fin = [];
 
-    for (let i = 0; i < n_webs-2; i++) {
+    for (let i = 0; i < n_webs - 2; i++) {
         let randomY = Math.floor(Math.random() * (400 - 40 + 1)); //puntoY del hilo1
-        let randomY2 = Math.floor(Math.random() * (200 - 40 + 1)) + randomY/2; //puntoY del hilo2
+        let randomY2 = Math.floor(Math.random() * (300 - 40 + 1)) + randomY / 6; //puntoY del hilo2
         catetoY = randomY2 - randomY;
+        if (randomY < randomY2) {
+            let web_inclined_thread = game.add.graphics(0, 0);
+            web_inclined_thread.lineStyle(3, 0x800080);
+            web_inclined_thread.moveTo(thread_pos_array[i], randomY); //thread_pos_array[i] es el puntoX del hilo1
+            web_inclined_thread.lineTo(thread_pos_array[i] + catetoX, randomY2); //thread_pos_array[i] + catetoX es el puntoX del hilo2
 
-        let web_inclined_thread = game.add.graphics(0, 0);
-        web_inclined_thread.lineStyle(3, 0x800080);
-        web_inclined_thread.moveTo(thread_pos_array[i], randomY); //thread_pos_array[i] es el puntoX del hilo1
-        web_inclined_thread.lineTo(thread_pos_array[i] + catetoX, randomY2); //thread_pos_array[i] + catetoX es el puntoX del hilo2
+            let point_ini = thread_changer_init.create(thread_pos_array[i], randomY, 'purple', i);
+            point_ini.myValue = i; //generar los puntos de inicio y final de hilos (sprites)
+            point_ini.anchor.setTo(0.5);
+            point_ini.scale.setTo(0.5);
+            let point_fin = thread_changer_end.create(thread_pos_array[i] + catetoX, randomY2, 'purple');
+            point_fin.anchor.setTo(0.5);
+            point_fin.scale.setTo(0.5);
 
-        let point_ini = thread_changer.create(thread_pos_array[i], randomY, 'purple'); //generar los puntos de inicio y final de hilos (sprites)
-        point_ini.anchor.setTo(0.5);
-        point_ini.scale.setTo(0.5);
-        let point_fin = thread_changer.create(thread_pos_array[i] + catetoX, randomY2,'purple');
-        point_fin.anchor.setTo(0.5);
-        point_fin.scale.setTo(0.5);
+            thread_inclined_array_ini.push([thread_pos_array[i], randomY]); //guardamos el PUNTO donde EMPIEZAN los hilos
+            thread_inclined_array_fin.push([thread_pos_array[i] + catetoX, randomY2]); //guardamos el PUNTO donde TERMINAN los hilos
+        }
 
-        thread_inclined_array_ini.push([thread_pos_array[i], randomY]); //guardamos el PUNTO donde EMPIEZAN los hilos
-        thread_inclined_array_fin.push([thread_pos_array[i] + catetoX, randomY2]); //guardamos el PUNTO donde TERMINAN los hilos
+
     }
 }
